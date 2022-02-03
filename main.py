287b0ae547d9049c0d31241b66d59ffa9160ef19
nico <nikiapple1008@icloud.com>
2022-02-04 00:08:35 +0300

    start

---
+++ main.py
@@ -0,0 +1,19 @@
+def array_check(a):
+    for elem in a:
+        if elem<5:
+            print(elem);
+
+def array_con(b,c):
+    print("a");
+
+
+
+
+
+
+
+
+
+
+print("начало")
+print(array_check([23,32,23,34,54,4,2,62,6,26,2,62,62,62]))
